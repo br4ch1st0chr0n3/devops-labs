d4a42024b63f517314d7c56ee261d4a59852cef0
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-01-20 17:51:06 +0300

    add: env2json

---
+++ env2json/flake.nix
@@ -0,0 +1,39 @@
+{ pkgs }:
+let
+  script = ./env-to-json.py;
+  mkJSON = dotenvPath: pkgs.stdenv.mkDerivation {
+    name = "make-json";
+    buildInputs = [ pkgs.python310 ];
+    unpackPhase = "true";
+    installPhase = ''
+      mkdir -p $out
+      python ${script} ${dotenvPath} > $out/.json
+    '';
+  };
+  envToJSONConverter =
+    let
+      name = "env-to-json-converter";
+    in
+    pkgs.stdenv.mkDerivation {
+      inherit name;
+      buildInputs = [ pkgs.python310 ];
+      unpackPhase = "true";
+      installPhase = ''
+        mkdir -p $out/bin
+        cp ${script} $out/bin/${name}
+        chmod +x $out/bin/${name}
+      '';
+    };
+  packages = {
+    default = envToJSONConverter;
+  };
+  lib = {
+    inherit mkJSON;
+  };
+  tests = {
+    mkJSONDemo = mkJSON ./app.env;
+  };
+in
+{
+  inherit packages lib tests;
+}
